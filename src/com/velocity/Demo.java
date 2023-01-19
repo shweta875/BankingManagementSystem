5f5295c8395e39813953b2c9a80634f7a1ddd4b4
shweta <shwetashinde875@gmail.com>
2023-01-19 13:28:18 +0530

    changes are successfully done

---
+++ src/com/velocity/Demo.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Demo {
+	public static void main(String[] args) {
+		ArrayList l=new ArrayList();
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		System.out.println(l);
+	}
+
+}
